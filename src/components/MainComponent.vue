cceb372d23324b03e48376b9314abdd6fe1a6eaa
jenja <zhenia.galanix@gmail.com>
2021-07-08 15:08:37 +0300

    add style to maincomponents

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -11,94 +11,113 @@
       </p>
     </div>
     <div class="section__main">
-      <input type="checkbox" id="shop-active" v-model="mainTab.shopActive" />
-      <label for="shop-active">Shop Active</label>
       <div class="section__main__wrapper">
-        <hk-form-group>
-          <hk-label required sign="*">Name</hk-label>
-          <hk-input
-            v-model="mainTab.nameInputValue"
-            name="name-input"
-            id="name-input"
+        <div class="flex">
+          <input
+            type="checkbox"
+            id="shop-active"
+            v-model="mainTab.shopActive"
           />
-        </hk-form-group>
+          <label for="shop-active">Shop Active</label>
+        </div>
         <hk-form-group>
-          <hk-label>Event Data</hk-label>
-          <hk-input-date
-            v-model="mainTab.time"
-            value-type="format"
-            format="YYYY-MM-DD"
-          ></hk-input-date>
+          <div class="width">
+            <hk-label required sign="*">Name</hk-label>
+            <hk-input
+              v-model="mainTab.nameInputValue"
+              name="name-input"
+              id="name-input"
+            />
+          </div>
+
+          <div class="width">
+            <hk-label>Event Data</hk-label>
+            <hk-input-date
+              v-model="mainTab.time"
+              value-type="format"
+              format="YYYY-MM-DD"
+            ></hk-input-date>
+          </div>
+
+          <div class="width">
+            <hk-label>Default Country</hk-label>
+            <hk-select
+              v-model="mainTab.selectedCountry"
+              :options="countrySelectList"
+              placeholder="FirstCountry"
+            />
+          </div>
+          <div class="width">
+            <hk-label>Background type</hk-label>
+            <hk-select
+              v-model="mainTab.selectedBackgroundType"
+              :options="backgroundTypes"
+              placeholder="Fill"
+            />
+          </div>
+          <div class="width">
+            <hk-label required sign="* ">Shop Style</hk-label>
+            <hk-select
+              v-model="mainTab.selectedShopStyle"
+              :options="shopStyleList"
+              placeholder="Black"
+            />
+          </div>
+
+          <div class="width">
+            <hk-label required sign="* ">Widget size</hk-label>
+            <hk-select
+              v-model="mainTab.selectedWidgetSize"
+              :options="widgetSize"
+              placeholder="Sq"
+            />
+          </div>
         </hk-form-group>
-        <hk-form-group>
-          <hk-label>Default Country</hk-label>
-          <hk-select
-            v-model="mainTab.selectedCountry"
-            :options="countrySelectList"
-            placeholder="FirstCountry"
-        /></hk-form-group>
-        <hk-form-group
-          ><hk-label>Background type</hk-label>
-          <hk-select
-            v-model="mainTab.selectedBackgroundType"
-            :options="backgroundTypes"
-            placeholder="Fill"
-        /></hk-form-group>
-        <hk-form-group>
-          <hk-label required sign="* ">Shop Style</hk-label>
-          <hk-select
-            v-model="mainTab.selectedShopStyle"
-            :options="shopStyleList"
-            placeholder="Black"
-          />
-        </hk-form-group>
-        <hk-form-group>
-          <hk-label required sign="* ">Widget size</hk-label>
-          <hk-select
-            v-model="mainTab.selectedWidgetSize"
-            :options="widgetSize"
-            placeholder="Sq"
-          />
-        </hk-form-group>
-        <hk-form-group>
-          <hk-label for="background-color-input">Background color</hk-label>
-          <hk-input
-            v-model="mainTab.backgroundColor"
-            name="background-color-input"
-            id="background-color-input"
-          />
-        </hk-form-group>
+        <div class="last-input">
+          <hk-form-group style="display: block; margin: 5%">
+            <hk-label for="background-color-input">Background color</hk-label>
+
+            <hk-input
+              v-model="mainTab.backgroundColor"
+              name="background-color-input"
+              id="background-color-input"
+            />
+          </hk-form-group>
+        </div>
+
         <div class="section__main__banner">
           <hk-form-group>
-            <hk-label>Banner</hk-label>
-            <div>
-              <hk-image-uploader
-                v-model="mainTab.imageBanner"
-                name="banner"
-              />
+            <div class="width">
+              <hk-label>Banner</hk-label>
+              <div>
+                <hk-image-uploader
+                  v-model="mainTab.imageBanner"
+                  name="banner"
+                />
+              </div>
             </div>
-          </hk-form-group>
-          <hk-form-group>
-            <hk-label>Banner bottom</hk-label>
-            <div>
-              <hk-image-uploader
-                v-model="mainTab.imageBannerBottom"
-                name="banner-bottom"
-              />
+            <div class="width">
+              <hk-label>Banner bottom</hk-label>
+              <div>
+                <hk-image-uploader
+                  v-model="mainTab.imageBannerBottom"
+                  name="banner-bottom"
+                />
+              </div>
             </div>
-          </hk-form-group>
-          <hk-form-group>
-            <hk-label>Banner top</hk-label>
-            <div>
-              <hk-image-uploader
-                v-model="mainTab.imageBannerTop"
-                name="banner-top"
-              />
+            <div class="width">
+              <hk-label>Banner top</hk-label>
+              <div>
+                <hk-image-uploader
+                  v-model="mainTab.imageBannerTop"
+                  name="banner-top"
+                />
+              </div>
             </div>
           </hk-form-group>
         </div>
         <div class="section__main__footer">
-          <hk-form-group>
+          <div class="width">
             <hk-label for="timer-input">Checkout timer (in minutes)</hk-label>
             <hk-input
               name="timer-input"
@@ -107,23 +126,25 @@
               v-model="mainTab.checkoutTimer"
             />
             <span>Default value 15 minutes</span>
-          </hk-form-group>
-          <hk-form-group>
+          </div>
+
+          <div class="flex">
             <input
               v-model="mainTab.displayEvent"
               type="checkbox"
               id="display-event"
             />
             <label for="display-event">Shop Active</label>
-          </hk-form-group>
-          <hk-form-group>
+          </div>
+
+          <div class="flex">
             <input
               v-model="mainTab.showOrderedPage"
               type="checkbox"
               id="show-order"
             />
             <label for="show-order">Show Order Conformation page</label>
-          </hk-form-group>
+          </div>
         </div>
       </div>
     </div>
@@ -175,7 +196,32 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+.last-input {
+  width: 33%;
+  .form-group {
+    display: block;
+    margin: 5%;
+    .pos-r {
+      display: flex;
+      .pos-r {
+        width: 90%;
+      }
+    }
+  }
+}
+.flex {
+  display: flex;
+  margin: 3% 0;
+}
+.width {
+  width: 30%;
+  margin: 3% 0;
+}
+.margin-and-center {
+  width: 96%;
+  margin: 0 auto;
+}
 .tab__wrapper {
   border: 1px solid black;
   .btn-vue-store {
@@ -183,13 +229,18 @@ export default {
     padding: 10px;
   }
   .section__main {
-    &__wrapper{
+    &__wrapper {
       //indentation and add to center wrapper
       width: 94%;
       margin: 0 auto;
-      .form-group{
-        display: inline-block;
-        width: 33%;
+      .form-group {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        flex-wrap: wrap;
+        .form-element {
+          width: 94%;
+        }
       }
     }
   }
